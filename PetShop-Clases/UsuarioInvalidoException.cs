5bd731cd469b44d43acaf91a90c640bb5315af14
Ivan-Stemberger <71237941+Ivan-Stemberger@users.noreply.github.com>
2021-10-17 15:30:14 -0300

    Excepciones mínimas agregadas.

---
+++ PetShop-Clases/UsuarioInvalidoException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop_Clases
+{
+    public class UsuarioInvalidoException:Exception
+    {
+        public override string Message
+        {
+            get 
+            {
+                return "Usuario o contraseña Invalidos.";
+            }
+        }
+    }
+}
